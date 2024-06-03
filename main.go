3567fd2dfcbc63b476539251094b9c3470ebc03b
Samuele Musiani <samu@teapot.ovh>
2024-06-03 12:29:49 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,181 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+const (
+	groupsFile          = "groups.json"
+	degreesFile         = "degrees.json"
+	teachingsFile       = "teachings.json"
+	timetablesFile      = "timetables.json"
+	maintainersFile     = "maintainers.json"
+	representativesFile = "representatives.json"
+)
+
+type Maintainer struct {
+	Id       int64  `json:"id"`
+	Username string `json:"username"`
+}
+
+// config/teachings.json
+
+type Teaching struct {
+	Name       string   `json:"name"`
+	Url        string   `json:"url"`
+	Chat       string   `json:"chat"`
+	Website    string   `json:"website"`
+	Professors []string `json:"professors"`
+}
+
+// config/degrees.json
+
+type YearStudyDiagram struct {
+	Mandatory []string `json:"mandatory"`
+	Electives []string `json:"electives"`
+}
+
+type Year struct {
+	Year      int64            `json:"year"`
+	Chat      string           `json:"chat"`
+	Teachings YearStudyDiagram `json:"teachings"`
+}
+
+type Degree struct {
+	Id    string `json:"id"`
+	Name  string `json:"name"`
+	Icon  string `json:"icon"`
+	Years []Year `json:"years"`
+	Chat  string `json:"chat"`
+}
+
+// timetables.json
+
+type Curriculum struct {
+	Name     string `json:"name"`
+	Callback string `json:"callback"`
+}
+
+// Recognized by a callback string
+type Timetable struct {
+	Course       string `json:"course"`    // Course title
+	Name         string `json:"name"`      // Course name
+	Type         string `json:"type"`      // Type (laurea|magistrale|2cycle)
+	Curriculum   string `json:"curricula"` // Curriculum
+	Title        string `json:"title"`
+	FallbackText string `json:"fallbackText"`
+}
+
+type RepresentativesData struct {
+	Description  string `json:"description"`
+	Title        string `json:"title"`
+	FallbackText string `json:"fallbackText"`
+}
+
+type Representative struct {
+	Course          string   `json:"course"`
+	Representatives []string `json:"representatives"`
+}
+
+func ParseTeachings(configPath string) (teachings []Teaching, err error) {
+	filepath := filepath.Join(configPath, teachingsFile)
+	file, err := os.Open(filepath)
+	defer file.Close()
+	if err != nil {
+		return nil, fmt.Errorf("error reading %s file: %w", filepath, err)
+	}
+
+	err = json.NewDecoder(file).Decode(&teachings)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing %s file: %w", filepath, err)
+	}
+	return
+}
+
+func ParseDegrees(configPath string) (degrees []Degree, err error) {
+	filepath := filepath.Join(configPath, degreesFile)
+	file, err := os.Open(filepath)
+	defer file.Close()
+	if err != nil {
+		return nil, fmt.Errorf("error reading %s file: %w", degreesFile, err)
+	}
+	err = json.NewDecoder(file).Decode(&degrees)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing %s file: %w", degreesFile, err)
+	}
+	return
+}
+
+func ParseTimetables(configPath string) (timetables map[string]Timetable, err error) {
+	filepath := filepath.Join(configPath, timetablesFile)
+	file, err := os.Open(filepath)
+	defer file.Close()
+	if err != nil {
+		return nil, fmt.Errorf("error reading %s file: %w", timetablesFile, err)
+	}
+
+	var mapData map[string]Timetable
+
+	err = json.NewDecoder(file).Decode(&mapData)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing %s file: %w", filepath, err)
+	}
+
+	timetables = mapData
+	return
+}
+
+func ParseMaintainers(configPath string) (maintainer []Maintainer, err error) {
+	filepath := filepath.Join(configPath, maintainersFile)
+	file, err := os.ReadFile(filepath)
+	if errors.Is(err, os.ErrNotExist) {
+		return maintainer, fmt.Errorf("%s does not exist", maintainersFile)
+	} else if err != nil {
+		return nil, fmt.Errorf("error reading %s file: %w", maintainersFile, err)
+	}
+
+	var projects []struct {
+		Name        string       `json:"project"`
+		Maintainers []Maintainer `json:"maintainers"`
+	}
+
+	err = json.Unmarshal(file, &projects)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing %s file: %w", maintainersFile, err)
+	}
+
+	for _, p := range projects {
+		if p.Name == "informabot" {
+			return p.Maintainers, nil
+		}
+	}
+
+	return nil, fmt.Errorf("couldn't found informabot projects after parsing %s", maintainersFile)
+}
+
+func ParseRepresentatives(configPath string) (map[string]Representative, error) {
+	representatives := make(map[string]Representative)
+
+	filepath := filepath.Join(configPath, representativesFile)
+	byteValue, err := os.ReadFile(filepath)
+	if errors.Is(err, os.ErrNotExist) {
+		return representatives, nil
+	} else if err != nil {
+		return nil, fmt.Errorf("error reading %s file: %w", filepath, err)
+	}
+
+	err = json.Unmarshal(byteValue, &representatives)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing %s file: %w", filepath, err)
+	}
+
+	if representatives == nil {
+		representatives = make(map[string]Representative)
+	}
+
+	return representatives, nil
+}
